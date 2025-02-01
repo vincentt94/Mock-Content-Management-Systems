733d3c45d283fa636714fcaa962ecf3139bc151b
vincentt94 <vincent.h.thao@gmail.com>
2025-02-01 15:39:24 -0600

    Add files via upload

---
+++ src/classes/roles.ts
@@ -0,0 +1,24 @@
+import pool from "../connection.js";
+
+class Role {
+    //this function will show all available roles
+    static async getAllRoles() {
+        const query = "SELECT id, title, salary FROM roles;";
+        const result = await pool.query(query);
+        return result.rows;
+    }
+    //this function will add roles 
+    static async addRole(title:string, salary:number, departmentId: number) {
+        const query = `
+            INSERT INTO roles (title, salary, department_id)
+            VALUES ($1, $2, $3)
+            RETURNING id;
+        `;
+        const values = [title, salary, departmentId];
+        const result = await pool.query(query, values);
+        return result.rows[0].id;
+    }
+
+}
+
+export default Role
